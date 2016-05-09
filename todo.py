93408626764462560f9600c142d9fbe81e440dd5
john mason <jmason@vendasta.com>
2016-05-09 16:07:44 -0600

    nothing to fix, problem was that google simulates eventual consistency. using strongly consistent data would solve the problem but that is overkill for this small project

--- todo.py
+++ todo.py
@@ -1,8 +1,14 @@
 import os
+import urllib
 
 import jinja2
 import webapp2
 from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class Entry(ndb.Model):
@@ -25,38 +31,30 @@ class Entry(ndb.Model):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        entry_list = Entry.query().fetch(100)
+        entry_list = Entry.query().fetch(10)
         template_values = {
-            'todo_list': [1, 2, 3],
             'entry_list': entry_list
         }
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
 
-class Test(webapp2.RequestHandler):
-    def get(self):
+class AddEntry(webapp2.RequestHandler):
+    def post(self):
         content = self.request.get('todo_entry')
         Entry.create_entry(content)
-        self.redirect('/')
+        self.redirect('/?')
 
 
 class DeleteEntry(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         entry_content = self.request.get('entry_to_delete')
         entry_to_delete = Entry.build_key(entry_content)
-
         entry_to_delete.delete()
-        self.redirect('/')
-
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+        self.redirect('/?')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/test', Test),
-    ('/delete', DeleteEntry)
+    ('/add_entry', AddEntry),
+    ('/delete_entry', DeleteEntry)
 ], debug=True)
